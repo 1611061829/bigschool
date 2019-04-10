08a63f4b31f8b93851d94a4b736cf9a3cc20bdfe
Administrator <Administrator@ADMIN>
2019-04-10 10:17:50 +0700

    asas

---
+++ BigSchool/Controllers/CoursesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using BigSchool.Models;
+
+namespace BigSchool.Controllers
+{
+    internal class CoursesViewModel
+    {
+        public IQueryable<Course> UpCommingCourses { get; set; }
+        public bool ShowAction { get; set; }
+    }
+}
