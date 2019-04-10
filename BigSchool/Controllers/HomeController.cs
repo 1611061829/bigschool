1291a236f054d8f8be1d6849094b8d0ed111d60a
Administrator <Administrator@ADMIN>
2019-04-10 10:29:41 +0700

    qqq

--- BigSchool/Controllers/HomeController.cs
+++ BigSchool/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace BigSchool.Controllers
         }
         //public ActionResult Index()
         //{
-        //    return View();
+        //    return View(); 
         //}
         public ActionResult Index()
         {
